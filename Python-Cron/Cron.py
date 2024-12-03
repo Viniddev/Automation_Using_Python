e9eb6e5fc946e08f5079f63311982b6793a64770
Vinícius Dias Rodrigues <107416723+Viniddev@users.noreply.github.com>
2024-12-03 11:10:40 -0300

    Update Cron.py

--- Python-Cron/Cron.py
+++ Python-Cron/Cron.py
@@ -1,13 +1,23 @@
 import os
 
 def organize_folder():
-    types = ["jpg", "png", "zip", "pdf", "svg"]
+    types = [
+        "jpg", "png", "zip", "pdf", "svg", "xlsx", "docx", "exe", "doc", "jpeg", "pptx", "json"
+    ]
 
     base_Path = os.path.expanduser("~")
-    path = os.path.join(base_Path, "Downloads")
+    path = os.path.join(base_Path, "Documents")
+
+    print(path)
 
     cwd = os.chdir(path)
     full_List = os.listdir(cwd)
+
+    for files in os.listdir():
+        document = files.split(".")
+        if len(document) > 1:
+            if document[len(document)-1] not in types:
+                types.append(document[1])
 
     for type_ in types:
         if type_ not in os.listdir():
@@ -23,4 +33,3 @@ def organize_folder():
 
 if __name__ == "__main__":
     organize_folder()
-
